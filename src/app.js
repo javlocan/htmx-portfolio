a016720ee2df084d26c659ffbea97f32b5d22e32
javlocan <javlocan.dev@gmail.com>
2023-09-21 11:19:41 +0200

    Trying from here... packages are outdated

--- src/app.js
+++ src/app.js
@@ -1,20 +1,32 @@
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-
-const todosRouter = require('./routes/todos');
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 
 const app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '..', 'public')));
+app.use(express.static(path.join(__dirname, "..", "public")));
 
 // Routes
-app.use('/todos', todosRouter);
 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+});
+
+app.get("/home", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "home", "index.html"));
+});
+
+app.get("/nav/home", (req, res) => {
+  res.sendFile(path.join(__dirname, "partials", "nav", "home", "index.html"));
+});
+
+app.get("/nav/about", (req, res) => {
+  res.sendFile(path.join(__dirname, "partials", "nav", "about", "index.html"));
+});
 
 module.exports = app;
